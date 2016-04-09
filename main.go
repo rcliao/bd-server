c927fce6273f23dabccc9f1295095f8e4019b085
Eric Liao <rcliao01@gmail.com>
2016-04-08 22:23:42 -0700

    add token end points

--- main.go
+++ main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
+	"github.com/labstack/echo/middleware"
+)
+
+type (
+	key struct {
+		key string
+	}
 )
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
 	e.Get("/hello", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	fmt.Printf("Starting Echo server at port 9000")
+	e.Post("/api/token", func(c echo.Context) error {
+		key := new(key)
+		if err := c.Bind(key); err != nil {
+			return err
+		}
+		data := []byte(key.key)
+		return c.JSON(http.StatusOK, fmt.Sprintf("%x", md5.Sum(data)))
+	})
+	fmt.Printf("Starting Echo server at port 9000\n")
 	e.Run(standard.New(":9000"))
 }
